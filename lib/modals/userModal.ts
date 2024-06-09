41d4a13788f2108d2bd0879e2abed4e6e945bf2f
Utkarsh Tiwari <128823414+Developer-Utkarsh@users.noreply.github.com>
2024-06-09 10:48:21 +0530

    Updated userModal.ts

--- lib/modals/userModal.ts
+++ lib/modals/userModal.ts
@@ -1,25 +1,53 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
+const MeetingDetailsSchema = new Schema({
+	title: String,
+	meetingId: String,
+	createdAt: Date,
+	image: String,
+	createdBy: String,
+	status: String,
+	joinedAt: String,
+	startsAt: String,
+	endedAt: Date,
+	isEnded: Boolean,
+	isStarted: Boolean,
+	isScheduled: Boolean,
+	scheduledAt: Date,
+});
+const PublicMeetingDetailsSchema = new Schema({
+	title: String,
+	meetingId: String,
+	createdAt: Date,
+	image: String,
+	createdBy: String,
+	status: String,
+	joinedAt: Date,
+	totalUsers: [MeetingDetailsSchema],
+	endedAt: Date,
+	isEnded: Boolean,
+	isStarted: Boolean,
+	isScheduled: Boolean,
+	scheduledAt: Date,
+});
+
 // User Schema
 const UserSchema = new Schema({
 	_id: { type: mongoose.Schema.Types.ObjectId, auto: true },
 	email: { type: String, required: true, unique: true, index: true },
 	image: { type: String },
-	createdAt: { type: String, default: new Date() },
-	updatedAt: { type: String, default: new Date()  },
-	totalMeetings: [
-		{ type: mongoose.Schema.Types.ObjectId, ref: "Meetings" },
-	],
-	createdMeetings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meetings" }],
-	publicMeetings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Pubic Meetings" }],
+	createdAt: { type: Date, default: new Date() },
+	updatedAt: { type: Date, default: new Date() },
+	totalMeetings: [MeetingDetailsSchema], // Changed to use MeetingDetailsSchema
+	createdMeetings: [MeetingDetailsSchema],
+	publicMeetings: [PublicMeetingDetailsSchema],
 	clerkId: { type: String, required: true, unique: true, index: true },
 	username: { type: String, unique: true, index: true },
 	firstName: { type: String },
 	lastName: { type: String },
+	isPrivate:{type:Boolean}
 });
 
-
-const User = models?.user || model("users", UserSchema);
-
+const User = models?.users || model("users", UserSchema);
 
 export { User };
